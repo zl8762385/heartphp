2d075708e0c1f205ea4fed652868a45d70aba46a
zl8762385 <zl8762385@163.com>
2013-03-17 04:17:23 +0800

    config.php

---
+++ conf/config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *  index.php 入口文件
+ *
+ * @copyright			(C) 20013-2015 HeartPHP
+ * @author              zhangxiaoliang
+ * @lastmodify			2013.03.17
+ *
+ * 您可以自由使用该源码，但是在使用过程中，请保留作者信息。尊重他人劳动成果就是尊重自己
+ */
+
